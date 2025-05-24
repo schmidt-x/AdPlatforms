ebf60288ee3333036bf3f1229d600f306fa55f9b
Stefano <schmidtsigma@gmail.com>
2025-05-24 19:49:50 +0300

    add parsing helpers with tests

---
+++ AdPlatforms.UnitTests/HelpersTests.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using AdPlatforms.Common;
+using AdPlatforms.Domain.Enums;
+
+namespace AdPlatforms.UnitTests;
+
+public class HelpersTests
+{
+	[Fact]
+	public void ShouldFailIfNoPlatformIsGiven()
+	{
+		// Arrange
+		const string input = "\n\n\n";
+		using var sr =  new StringReader(input);
+		
+		// Act
+		var result = Helpers.ParsePlatforms(sr);
+		
+		// Assert
+		Assert.True(result.IsError(out var error, out _));
+		Assert.Equal(ErrorCode.EmptyPlatform, error.Code);
+		Assert.Equal("At least one Platform is required.", error.Description);
+	}
+	
+	[Fact]
+	public void ShouldFailIfColonIsMissing()
+	{
+		// Arrange
+		const string input = "Platform /location";
+		using var sr =  new StringReader(input);
+		
+		// Act
+		var result = Helpers.ParsePlatforms(sr);
+		
+		// Assert
+		Assert.True(result.IsError(out var error, out _));
+		Assert.Equal(ErrorCode.InvalidValue, error.Code);
+		Assert.Equal("Platform and Location(s) must be separated by a colon ':'. Line: 1.", error.Description);
+	}
+	
+	[Fact]
+	public void ShouldFailIfPlatformIsEmpty()
+	{
+		// Arrange
+		const string input = ":/location";
+		using var sr =  new StringReader(input);
+		
+		// Act
+		var result = Helpers.ParsePlatforms(sr);
+		
+		// Assert
+		Assert.True(result.IsError(out var error, out _));
+		Assert.Equal(ErrorCode.EmptyPlatform, error.Code);
+		Assert.Equal("Platform name is required. Line: 1.", error.Description);
+	}
+	
+	[Fact]
+	public void ShouldFailIfLocationIsEmpty()
+	{
+		// Arrange
+		const string input = "Platform:";
+		using var sr =  new StringReader(input);
+		
+		// Act
+		var result = Helpers.ParsePlatforms(sr);
+		
+		// Assert
+		Assert.True(result.IsError(out var error, out _));
+		Assert.Equal(ErrorCode.EmptyLocation, error.Code);
+		Assert.Equal("At least one Location is required. Line: 1.", error.Description);
+	}
+	
+	[Fact]
+	public void ShouldFailIfLocationContainsNoElements()
+	{
+		// Arrange
+		const string input = "Platform:,,,";
+		using var sr =  new StringReader(input);
+		
+		// Act
+		var result = Helpers.ParsePlatforms(sr);
+		
+		// Assert
+		Assert.True(result.IsError(out var error, out _));
+		Assert.Equal(ErrorCode.EmptyLocation, error.Code);
+		Assert.Equal("At least one Location is required. Line: 1.", error.Description);
+	}
+	
+	[Theory]
+	[InlineData("Platform:/", 1)]
+	[InlineData("Platform:/en\r\nPlatform2:/en,//", 2)]
+	public void ShouldFailIfLocationElementConsistsOfEmptySegments(string input, int line)
+	{
+		// Arrange
+		using var sr =  new StringReader(input);
+		
+		// Act
+		var result = Helpers.ParsePlatforms(sr);
+		
+		// Assert
+		Assert.True(result.IsError(out var error, out _));
+		Assert.Equal(ErrorCode.EmptyLocation, error.Code);
+		Assert.Equal($"Location must contain at least one non-empty segment. Line: {line}.", error.Description);
+	}
+}
---
+++ AdPlatforms/Common/Helpers.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AdPlatforms.Domain.Enums;
+using AdPlatforms.Domain.Models;
+
+namespace AdPlatforms.Common;
+
+public static class Helpers
+{
+	public static Result<IReadOnlyCollection<Platform>, Error> ParsePlatforms(TextReader textReader)
+	{
+		List<Platform> platforms = [];
+		
+		int lineIndex = 0;
+		while (textReader.ReadLine() is { } rawLine)
+		{
+			lineIndex++;
+			ReadOnlySpan<char> line = rawLine;
+			
+			line = line.Trim();
+			if (line.IsEmpty) continue;
+			
+			int index = line.IndexOf(':');
+			if (index == -1)
+			{
+				return new Error(ErrorCode.InvalidValue, $"Platform and Location(s) must be separated by a colon ':'. Line: {lineIndex}.");
+			}
+			
+			var platform = line[..index].Trim();
+			if (platform.IsEmpty)
+			{
+				return new Error(ErrorCode.EmptyPlatform, $"Platform name is required. Line: {lineIndex}.");
+			}
+			
+			var rawLocations = line[(index+1)..];
+			if (rawLocations.IsEmpty)
+			{
+				return new Error(ErrorCode.EmptyLocation, $"At least one Location is required. Line: {lineIndex}.");
+			}
+			
+			List<Location> locations = [];
+			
+			foreach (var locationRange in rawLocations.Split(','))
+			{
+				var rawLocation = rawLocations[locationRange].Trim();
+				
+				if (rawLocation.IsEmpty) continue; // Empty entries are allowed (e.g., "Platform:,,,/a/b/c,,").
+				
+				if (ParseLocation(rawLocation).IsError(out Error? error, out Location? location))
+				{
+					return error with { Description = $"{error.Description} Line: {lineIndex}." };
+				}
+				
+				locations.Add(location);
+			}
+			
+			if (locations.Count == 0)
+			{
+				return new Error(ErrorCode.EmptyLocation, $"At least one Location is required. Line: {lineIndex}.");
+			}
+			
+			platforms.Add(new Platform(platform.ToString(), locations));
+		}
+		
+		return platforms.Count != 0
+			? platforms
+			: new Error(ErrorCode.EmptyPlatform, "At least one Platform is required.");
+	}
+	
+	public static Result<Location, Error> ParseLocation(ReadOnlySpan<char> location)
+	{
+		List<string> segments = [];
+		
+		foreach (var segmentRange in location.Split('/'))
+		{
+			var segment = location[segmentRange].Trim();
+			
+			if (segment.IsEmpty) continue; // Empty segments are allowed (e.g., "Platform:///a/b/c//").
+			
+			foreach (var ch in segment)
+			{
+				// TODO: validate for restricted characters
+			}
+			
+			segments.Add(segment.ToString());
+		}
+		
+		return segments.Count != 0
+			? new Location(segments)
+			: new Error(ErrorCode.EmptyLocation, "Location must contain at least one non-empty segment.");
+	}
+}
