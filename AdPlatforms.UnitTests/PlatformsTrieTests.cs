bb2e5f4e4b19244c5b5978c43c36bfba8ea58c20
Stefano <schmidtsigma@gmail.com>
2025-05-24 14:59:40 +0300

    add PlatformTrie and tests

---
+++ AdPlatforms.UnitTests/PlatformsTrieTests.cs
@@ -0,0 +1,52 @@
+﻿using AdPlatforms.Common.Structures;
+
+namespace AdPlatforms.UnitTests;
+
+public class PlatformsTrieTests
+{
+	[Theory]
+	[InlineData(new[] { "ru" },                   new[] { "Яндекс.Директ" })]
+	[InlineData(new[] { "ru", "svrd" },           new[] { "Яндекс.Директ", "Крутая реклама" })]
+	[InlineData(new[] { "ru", "msk" },            new[] { "Яндекс.Директ", "Газета уральских москвичей" })]
+	[InlineData(new[] { "ru", "permobl" },        new[] { "Яндекс.Директ", "Газета уральских москвичей" })]
+	[InlineData(new[] { "ru", "chelobl" },        new[] { "Яндекс.Директ", "Газета уральских москвичей" })]
+	[InlineData(new[] { "ru", "svrd", "revda" },  new[] { "Яндекс.Директ", "Крутая реклама", "Ревдинский рабочий" })]
+	[InlineData(new[] { "ru", "svrd", "pervik" }, new[] { "Яндекс.Директ", "Крутая реклама", "Ревдинский рабочий" })]
+	public void ShouldReturnExpectedPlatforms(string[] searchLocation, string[] expectedPlatforms)
+	{
+		// Arrange
+
+		// Яндекс.Директ:/ru 
+		// Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik 
+		// Газета уральских москвичей:/ru/msk,/ru/permobl,/ru/chelobl 
+		// Крутая реклама:/ru/svrd 
+		
+		(string Platform, string[] LocationSegments)[] input = [
+			("Яндекс.Директ",              [ "ru" ]),
+			("Ревдинский рабочий",         [ "ru", "svrd", "revda" ]),
+			("Ревдинский рабочий",         [ "ru", "svrd", "pervik" ]),
+			("Газета уральских москвичей", [ "ru", "msk" ]),
+			("Газета уральских москвичей", [ "ru", "permobl" ]),
+			("Газета уральских москвичей", [ "ru", "chelobl" ]),
+			("Крутая реклама",             [ "ru", "svrd" ]),
+		];
+		
+		var trie = new PlatformTrie();
+		
+		foreach (var item in input)
+		{
+			trie.Add(item.Platform, item.LocationSegments);
+		}
+		
+		// Act
+		
+		var actualPlatforms = trie.Find(searchLocation);
+		
+		// Assert
+		
+		Assert.Equal(expectedPlatforms.Length, actualPlatforms.Count);
+		Assert.Equal(expectedPlatforms, actualPlatforms);
+	}
+	
+	// TODO: test edge cases
+}
---
+++ AdPlatforms/Common/Structures/PlatformTrie.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AdPlatforms.Common.Structures;
+
+internal class PlatformTrie
+{
+	private readonly TrieNode _root = new(string.Empty);
+	
+	internal void Add(string platform, IReadOnlyCollection<string> locationSegments)
+	{
+		Debug.Assert(locationSegments.Count > 0, "Root node cannot have Platforms.");
+		
+		var current = _root;
+		
+		foreach (var segment in locationSegments)
+		{
+			if (current.Children.Count == 0 || !current.Children.TryGetValue(segment, out var child))
+			{
+				child = new TrieNode(segment);
+				current.Children.Add(segment, child);
+			}
+			
+			current = child;
+		}
+		
+		current.Platforms.Add(platform); // Here 'current' is the last node.
+	}
+	
+	internal IReadOnlyCollection<string> Find(IReadOnlyCollection<string> locationSegments)
+	{
+		List<string> platforms = [];
+		var current = _root;
+		
+		foreach (var segment in locationSegments)
+		{
+			if (current.Children.Count == 0 || !current.Children.TryGetValue(segment, out var child))
+				break;
+			
+			platforms.AddRange(child.Platforms);
+			current = child;
+		}
+		
+		return platforms;
+	}
+	
+	private class TrieNode(string locationSegment)
+	{
+		public string LocationSegment { get; } = locationSegment;
+		public List<string> Platforms { get; } = [];
+		public Dictionary<string, TrieNode> Children { get; } = [];
+	}
+}
