3d3b13f2968b27f189b2a2e3abfd4d891cae0bfe
Stefano <schmidtsigma@gmail.com>
2025-05-24 22:08:50 +0300

    add endpoints

---
+++ AdPlatforms/Endpoints/Platform.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using AdPlatforms.Services;
+using AdPlatforms.Infra;
+using AdPlatforms.Responses;
+using AdPlatforms.Responses.Results;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace AdPlatforms.Endpoints;
+
+public class Platform : EndpointGroupBase
+{
+	public override void Map(WebApplication app)
+	{
+		var group = app.MapGroup("platforms");
+		
+		group.MapPost(string.Empty, SavePlatforms)
+			.WithSummary("Saves platforms and locations")
+			.Produces(StatusCodes.Status201Created)
+			.Produces<ErrorResponse>(StatusCodes.Status400BadRequest)
+			.Produces(StatusCodes.Status415UnsupportedMediaType)
+			.DisableAntiforgery();
+		
+		group.MapGet("{*location}", GetPlatforms)
+			.WithSummary("Retrieves platforms for a given location")
+			.Produces<PlatformResponse>()
+			.Produces<ErrorResponse>(StatusCodes.Status400BadRequest);
+	}
+	
+	public static IResult SavePlatforms(IFormFile file, IPlatformService platformService)
+	{
+		if (!file.ContentType.Equals("text/plain", StringComparison.OrdinalIgnoreCase))
+		{
+			return CustomResults.UnsupportedMediaType();
+		}
+		
+		using Stream readStream = file.OpenReadStream();
+		
+		var error = platformService.Save(readStream);
+		
+		return error is null
+			? Results.Created()
+			: Results.BadRequest(new ErrorResponse(error));
+	}
+	
+	public static IResult GetPlatforms(string location, IPlatformService platformService)
+	{
+		location = HttpUtility.UrlDecode(location);
+		
+		return platformService.Get(location)
+			.Match<IResult>(
+				platforms => Results.Ok(new PlatformResponse(location, platforms)),
+				error => Results.BadRequest(new ErrorResponse(error)));
+	}
+}
